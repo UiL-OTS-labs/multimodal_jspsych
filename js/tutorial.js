7f41d4f36a19b3d4f2bb806a1128a8545bf05c96
Jacco van Elst <j.c.vanelst@uu.nl>
2020-08-06 16:39:15 +0200

    dude, i'm actually making a plugin ;-)

--- js/tutorial.js
+++ js/tutorial.js
@@ -134,7 +134,7 @@ var learn_trial_info_one = [
 //A1p, A2p, A3p, B1p, B2p, B3p, C1p, C2p, C3p
 var nine_trial_info = [
     {
-        target: "a1",
+        target: "_a1",
         target_image: "_image/_a1i.png",
         target_audio: "_audio/_a1s.wav",
         target_video: "_video/ma1n.mp4",
@@ -152,7 +152,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {
-        target: "a2",
+        target: "_a2",
         target_image: "_image/_a2i.png",
         target_audio: "_audio/_a2s.wav",
         target_video: "_video/ma2n.mp4",
@@ -170,7 +170,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {
-        target: "a3",
+        target: "_a3",
         target_image: "_image/_a3i.png",
         target_audio: "_audio/_a3s.wav",
         target_video: "_video/ma4n.mp4",
@@ -188,7 +188,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },  
     {  
-        target: "b1",
+        target: "_b1",
         target_image: "_image/_b1i.png",
         target_audio: "_audio/_b1s.wav",
         target_video: "_video/ne1n.mp4",        
@@ -206,7 +206,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {
-        target: "b2",
+        target: "_b2",
         target_image: "_image/_b2i.png",
         target_audio: "_audio/_b2s.wav",
         target_video: "_video/ne2n.mp4", 
@@ -224,7 +224,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {
-        target: "b3",
+        target: "_b3",
         target_image: "_image/_b3i.png",
         target_audio: "_audio/_b3s.wav",
         target_video: "_video/ne4n.mp4", 
@@ -242,7 +242,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {  
-        target: "c1",
+        target: "_c1",
         target_image: "_image/_c1i.png",
         target_audio: "_audio/_c1s.wav",
         target_video: "_video/yi1n.mp4", 
@@ -260,7 +260,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {
-        target: "c2",
+        target: "_c2",
         target_image: "_image/_c2i.png",
         target_audio: "_audio/_c2s.wav",
         target_video: "_video/yi2n.mp4",        
@@ -278,7 +278,7 @@ var nine_trial_info = [
         task_part: "test_learn_nine"
     },
     {
-        target: "c3",
+        target: "_c3",
         target_image: "_image/_c3i.png",
         target_audio: "_audio/_c3s.wav",
         target_video: "_video/yi4n.mp4", 
@@ -297,49 +297,68 @@ var nine_trial_info = [
     }   
 ];
 
-// var nine_button_fixed_pairs: [
-//             {0:"_image/_a1i.png"},
-//             {1:"_image/_a2i.png"},
-//             {2:"_image/_a3i.png"},
-//             {3:"_image/_b1i.png"},
-//             {4,"_image/_b2i.png"},
-//             {5,"_image/_b3i.png"},
-//             {6,"_image/_c1i.png"},
-//             {7,"_image/_c2i.png"},
-//             {8,"_image/_c3i.png"}
-//             ]
-//             
-//         target_image: "_image/_c3i.png",
-//         target_audio: "_audio/_c3s.wav",
-//         other_im_1:"_image/_a1i.png",
-//         other_im_2:"_image/_a2i.png",
-//         other_im_3:"_image/_a3i.png",
-//         other_im_4:"_image/_b1i.png",
-//         other_im_5:"_image/_b2i.png",
-//         other_im_6:"_image/_b3i.png",
-//         other_im_7:"_image/_c1i.png",
-//         other_im_8:"_image/_c2i.png",
-//         choices:["_a1","_a2","_a3","_b1","_b2","_b3","_c1","_c2","_c3"],
-//         correct_response:8,
-//         condition: "Tone 4",
-//         task_part: "test_learn_nine"
-//     }   
+
+//https://css-tricks.com/snippets/javascript/select-random-item-array/
+
+// var myArray = [
+//   {"0", "Apple"},
+//   {"1", "Banana"},
+//   {"2", "Pear"},
+//   {"3", "Orange"}
 // ];
-// var button_downsize = function{
-//     var button = image-btn;
-//     button.style.height = '100px';
-//     return button
+
+/**
+ * [someFunction description]
+ * @param  {array}       Strings with choices]
+ * @param  {string}      String with the correct choice
+ * @return {[type]}      [description]
+ */
+
+// function shuffle(array) {
+//   var currentIndex = array.length, temporaryValue, randomIndex;
+
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+
+//     // And swap it with the current element.
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
+
+//   return array;
 // }
 
-// let ranks = [1, 5, 7, 8, 10, 7];
-// let index = ranks.findIndex(rank => rank === 7);
-// console.log(index);
+// function shuffleChoiceButtons(choices, correct_choice_string) {
+//   //check
+//   console.log('pre-shuffled array:');
+//   console.log(choices)
+  
+//   //spec
+//   const oriIndex = choices.indexOf(correct_choice_string);
+
+//   //check
+//   console.log('oriIndex: ');
+//   console.log(oriIndex);
+  
+//   const betterChoices = shuffle(choices);
+  
+//   //check
+//   console.log('post-shuffled:');
+//   console.log(betterChoices);
+
+//   var newIndex = betterChoices.indexOf(correct_choice_string);
+//   console.log(newIndex);
+
+//   return {
+//     choiceList: betterChoices,
+//     index: newIndex
+//   };
+// }
 
 
-// let ranks = [1, 5, 7, 8, 10, 7];
 
-// let index = ranks.findIndex(
-//     (rank, index) => rank === 7 && index > 2
-// );
-
-// console.log(index);
